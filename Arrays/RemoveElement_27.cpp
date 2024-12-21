c1cc5f31ddc0e39150011baa934e7f1299206ef7
Aarthi Honguthi <aarthihonguthi025@gmail.com>
2024-12-21 10:20:55 +0530

    Arrays03

--- Arrays/RemoveElement_27.cpp
+++ Arrays/RemoveElement_27.cpp
@@ -22,9 +22,7 @@ sort(nums, 0, k); // Sort the first k elements of nums
 for (int i = 0; i < actualLength; i++) {
     assert nums[i] == expectedNums[i];
 }
-If all assertions pass, then your solution will be accepted.
-
-
+If all assertions pass, then your solution will be accepted
 
 Example 1:
 
